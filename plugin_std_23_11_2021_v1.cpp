62695c7fc8e5b798f0c1a21db818550d4f9c3d92
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2021-11-26 11:11:23 +0300

    Update plugin_std_23_11_2021_v1.cpp

--- plugin_std_23_11_2021_v1.cpp
+++ plugin_std_23_11_2021_v1.cpp
@@ -167,13 +167,14 @@ struct PAppInterface {
     // set everything to nullptr here if you don't support shaders
     struct {
         void (*apply)(void *shader, const PRenderMode*render_mode);
-        void *(*compile)(const char *code);
-        void (*release)(void *);
 
-        void (*set_uniform_int)(const char *name, int  val);
+        void *(*compile)(const char *code);
+        void  (*release)(void *);
+
+        void (*set_uniform_int)    (const char *name, int  val);
         void (*set_uniform_int_arr)(const char *name, int *val, size_t cnt);
 
-        void (*set_uniform_float)(const char *name, float  val);
+        void (*set_uniform_float)    (const char *name, float  val);
         void (*set_uniform_float_arr)(const char *name, float *val, size_t cnt);
 
         void (*set_uniform_tex)(const char *name, void *texture);
