05ff5d15d02c48c716e021d0e644911fb9134e98
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2021-12-08 18:59:28 +0300

    upd

--- plugin_std.hpp
+++ plugin_std.hpp
@@ -5,6 +5,8 @@
 #include <string>
 
 #include "types_std.h"
+#include "widget_std.h"
+#include "widgets/collection.h"
 
 
 // this function is only defined in plugin. call it to get PluginInterface to interact with plugin
@@ -14,24 +16,14 @@
 #if __cplusplus >= 201703L
 
 constexpr char PGET_INTERFACE_FUNC[] = "get_plugin_interface";
-constexpr uint32_t PSTD_VERSION = 0;
-
-constexpr char PST_TEXT_LINE[]    = "pst_text_line";
-constexpr char PST_SLIDER_1D[]    = "pst_slider_1d";
-constexpr char PST_SLIDER_2D[]    = "pst_slider_2d";
-constexpr char PST_COLOR_PICKER[] = "pst_color_picker";
+constexpr uint32_t PSTD_VERSION = 0x00010000;
 
 constexpr char PEXT_STD[] = "std";
 
 #else
 
 #define PGET_INTERFACE_FUNC "get_plugin_interface"
-#define PSTD_VERSION 0
-
-#define PST_TEXT_LINE    "pst_text_line"
-#define PST_SLIDER_1D    "pst_slider_1d"
-#define PST_SLIDER_2D    "pst_slider_2d"
-#define PST_COLOR_PICKER "pst_color_picker"
+#define PSTD_VERSION 0x00010000
 
 #define PEXT_STD  "std"
 
@@ -80,53 +72,61 @@ struct PluginInterface {
 
     virtual void apply() = 0;
 
-    virtual void on_press(Vec2f mouse_pos) = 0;
-    virtual void on_release(Vec2f mouse_pos) = 0;
-    virtual void on_move(Vec2f mouse_old_pos, Vec2f mouse_new_pos) = 0;
+    virtual void on_press  (Vec2f position) = 0;
+    virtual void on_release(Vec2f position) = 0;
+    virtual void on_move   (Vec2f from, Vec2f to) = 0;
 };
 
-struct PAppInterface {
+struct WidgetFactory {
+    virtual Button      *button       (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual Slider      *slider       (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual TextField   *text_field   (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual Window      *window       (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual ColorPicker *color_picker (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual Label       *label        (const WBody &body, Widget *parent = nullptr) = 0;
+    virtual Widget      *abstract     (const WBody &body, Widget *parent = nullptr) = 0;
+};
+
+struct ShaderFactory {
+    virtual Shader *compile(const char *code, ShaderType type) = 0;
+    virtual void    release(Shader *) = 0;
+};
+
+struct RenderTargetFactory {
+    virtual RenderTarget *spawn(size_t width, size_t height, RGBA color = {0, 0, 0, 255}) const = 0; // color -> fill with it
+    virtual void release(RenderTarget *target) const = 0;
+};
+
+struct AppInterface {
     uint32_t std_version;
     void *reserved;
 
-    // enables specified extension
-    virtual bool enable(std::string_view name) = 0;
-
-    // returns given function, if it is implemented in the specified (enabled) extension
-    virtual void *get_func(std::string_view extension, std::string_view func) = 0;
-    
     int feature_level;
 
-    virtual void log(const char *fmt, ...) = 0;
-    virtual double get_absolute_time() = 0;
+    struct {
+        WidgetFactory       *widget;
+        ShaderFactory       *shader;
+        RenderTargetFactory *target;
+    } factory;
 
-    virtual void release_pixels(RGBA *pixels) = 0;
+// extension
+    // enables specified extension
+    virtual bool enable(std::string_view name) const = 0;
+
+    // returns given function, if it is implemented in the specified (enabled) extension
+    virtual void *get_func(std::string_view extension, std::string_view func) const = 0;
+
+// general
+    virtual void log(const char *fmt, ...) const = 0;
+    virtual double get_absolute_time() const     = 0;
 
     virtual RGBA get_color() = 0;
     virtual float get_size() = 0;
 
-    virtual RenderTarget *get_target();
-    virtual RGBA *get_target_pixels();
-    virtual void get_size(size_t *width, size_t *height);
-
-    virtual void render_circle(Vec2f position, float radius, RGBA color, const RenderMode *render_mode) = 0;
-    virtual void render_line(Vec2f start, Vec2f end, RGBA color, const RenderMode *render_mode) = 0;
-    virtual void render_triangle(Vec2f p1, Vec2f p2, Vec2f p3, RGBA color, const RenderMode *render_mode) = 0;
-    virtual void render_rectangle(Vec2f p1, Vec2f p2, RGBA color, const RenderMode *render_mode) = 0;
-
-    virtual void render_pixels(Vec2f position, const RGBA *data, size_t width, size_t height, const RenderMode *render_mode) = 0;
-
-    // set everything to nullptr here if you don't support shaders
-    virtual void shader_apply(const RenderMode *render_mode);
-
-    virtual void *shader_compile(const char *code, ShaderType type);
-    virtual void  shader_release(void *);
-
-    virtual void shader_set_uniform_int      (void *shader, const char *name, int  val);
-    virtual void shader_set_uniform_int_arr  (void *shader, const char *name, int *val, size_t cnt);
-
-    virtual void shader_set_uniform_float    (void *shader, const char *name, float  val);
-    virtual void shader_set_uniform_float_arr(void *shader, const char *name, float *val, size_t cnt);
+// target
+    virtual RenderTarget *get_target() const  = 0;
+    virtual RenderTarget *get_preview() const = 0;
+    virtual void flush_preview() const = 0;
 };
 
 }
--- types_std.h
+++ types_std.h
@@ -78,18 +78,41 @@ union RGBA {
     RGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255) : r(r), g(g), b(b), a(a) {}
 };
 
-struct RenderMode {
-    BlendMode blend;
-    DrawPolicy draw_policy;
-    void *shader;
-};
-
 struct RenderTarget {
     size_t width;
     size_t height;
 
     virtual RGBA get_pixel(size_t x, size_t y) = 0;
     virtual void set_pixel(size_t x, size_t y, RGBA color) = 0;
+
+    virtual RGBA *get_pixels() = 0;
+
+    virtual void clear(RGBA color = 0) = 0; // fills the target with `color`
+
+// render
+    virtual void render_circle(Vec2f position, float radius, RGBA color, const RenderMode *render_mode) = 0;
+    virtual void render_line(Vec2f start, Vec2f end, RGBA color, const RenderMode *render_mode) = 0;
+    virtual void render_triangle(Vec2f p1, Vec2f p2, Vec2f p3, RGBA color, const RenderMode *render_mode) = 0;
+    virtual void render_rectangle(Vec2f p1, Vec2f p2, RGBA color, const RenderMode *render_mode) = 0;
+    
+    virtual void render_target(Vec2f position, const RGBA *data, size_t width, size_t height, const RenderMode *render_mode) = 0;
+    virtual void render_pixels(Vec2f position, const RGBA *data, size_t width, size_t height, const RenderMode *render_mode) = 0;
+
+    virtual void apply(const Shader *shader) = 0;
+};
+
+struct Shader {
+    virtual void shader_set_uniform_int      (void *shader, const char *name, int  val) = 0;
+    virtual void shader_set_uniform_int_arr  (void *shader, const char *name, int *val, size_t cnt) = 0;
+
+    virtual void shader_set_uniform_float    (void *shader, const char *name, float  val) = 0;
+    virtual void shader_set_uniform_float_arr(void *shader, const char *name, float *val, size_t cnt) = 0;
+};
+
+struct RenderMode {
+    BlendMode blend;
+    DrawPolicy draw_policy;
+    Shader *shader;
 };
 
 }
